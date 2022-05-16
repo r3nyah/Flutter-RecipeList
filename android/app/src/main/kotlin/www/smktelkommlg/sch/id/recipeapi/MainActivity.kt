53dc8099907b5b4d80cfbd1e57d4f6e7df7e29e1
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-05-16 16:18:10 +0700

    [📜]recipe_card.dart

---
+++ android/app/src/main/kotlin/www/smktelkommlg/sch/id/recipeapi/MainActivity.kt
@@ -0,0 +1,6 @@
+package www.smktelkommlg.sch.id.recipeapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Food Recipe',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: Scaffold(),
+    );
+  }
+}
