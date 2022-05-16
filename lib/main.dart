32a26b1eb64e952490b149780ba0e6647d36096d
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-05-16 19:10:14 +0700

    [📜]main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:recipeapi/views/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food Recipe',
+      title: 'Food recipe',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget{
           bodyText2: TextStyle(color: Colors.white),
         ),
       ),
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
